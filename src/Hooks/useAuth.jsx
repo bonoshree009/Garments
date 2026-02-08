707e0f97cb1aa6f5e6b9b0b0e14a8f2d5e1da7de
bonoshree009 <bonoshree.sarkarbd@gmail.com>
2026-02-09 02:13:42 +0600

    Auth

--- src/Hooks/useAuth.jsx
+++ src/Hooks/useAuth.jsx
@@ -1,3 +1,10 @@
 import { useContext } from "react";
 import { AuthContext } from "../Providers/AuthProvider";
 
+
+// useAuth hook
+const useAuth = () => {
+  return useContext(AuthContext);
+};
+
+export default useAuth;
