96f87992a701e1b149baaa19fd8362cd3e3bf444
bonoshree009 <bonoshree.sarkarbd@gmail.com>
2026-02-12 01:13:42 +0600

    How it works

--- src/Pages/Home/HowItWorks.jsx
+++ src/Pages/Home/HowItWorks.jsx
@@ -1,3 +1,5 @@
+import "../../App.css";
+
 const steps = [
   "Place Order",
   "Production Starts",
@@ -8,19 +10,19 @@ const steps = [
 
 const HowItWorks = () => {
   return (
-    <section className="bg-gray-100 py-16">
-      <div className="max-w-7xl mx-auto px-6">
-        <h2 className="text-3xl font-bold text-center mb-10">
+    <section className="bg-gray-100 py-16 ">
+      <div className="max-w-7xl mx-auto px-6 ">
+        <h2 className="text-3xl font-bold text-center mb-10 text-red-600">
           How It Works
         </h2>
 
         <div className="grid md:grid-cols-5 gap-6 text-center">
           {steps.map((step, index) => (
-            <div key={index} className="bg-white p-6 rounded-xl shadow">
-              <span className="text-2xl font-bold text-indigo-600">
+            <div key={index} className=" p-6 rounded-xl shadow bg-amber-300 card">
+              <h2> <span className="text-xl font-bold text-orange-600">
                 {index + 1}
-              </span>
-              <p className="mt-2 font-medium">{step}</p>
+              </span> </h2>
+              <h2 className="mt-2 font-bold text-2xl text-white">{step}</h2>
             </div>
           ))}
         </div>
